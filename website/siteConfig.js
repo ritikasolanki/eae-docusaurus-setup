bf50ad0b11e756494cb399970e6ebcacbbabf091
Ritika Solanki <46310337+ritikasolanki@users.noreply.github.com>
2020-05-28 16:44:49 +0530

    inital commit

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'EAE - Docusaurus', // Title for your website.
-  //tagline: 'A website for testing',
-  url: 'https://eae-docusaurus-setup.netlify.com', // Your website URL
-  baseUrl: '/eae-docusaurus-setup/', // Base URL for your project */
+  title: 'Test Site', // Title for your website.
+  tagline: 'A website for testing',
+  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'eae-docusaurus-setup',
-  organizationName: 'ritikasolanki',
+  projectName: 'test-site',
+  organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -54,8 +54,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#9b8122',
-    secondaryColor: '#6c5a17',
+    primaryColor: '#666b84',
+    secondaryColor: '#474a5c',
   },
 
   /* Custom fonts for website */
